80256b129bb5f94a7e6893ce8faedead605d2fc6
Smeu Elena <253783@via.dk>
2024-02-27 22:28:09 +0100

    singleton for connections

--- services/db.js
+++ services/db.js
@@ -1,9 +1,18 @@
 const mysql = require('mysql2/promise');
 const config = require('../config');
+const poolManager = require('mysql-connection-pool-manager');
+
+
+let connection;
+
 
 async function query(sql, params) {
-    const connection = await mysql.createPool(config.db);
+    if (!connection) {
+        connection = await mysql.createConnection(config.db);
+    }
     const [results, ] = await connection.execute(sql, params);
+    connection.end();
+    connection = null;
     return results;
 }
 
